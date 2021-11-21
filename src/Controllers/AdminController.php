df18269b639f123df96a7fd92911e7cea1aee88a
ChristianGroeber <christian.groeber@bluewin.ch>
2021-11-21 14:45:24 +0100

    getting raw page data

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -13,16 +13,32 @@ class AdminController extends AbstractController
     public function __construct(Nacho $nacho)
     {
         parent::__construct($nacho);
-        if (!$this->isGranted('Editor')) {
-            header('Http/1.1 302');
-            header('Location: /login?required_page=' . $_SERVER['REDIRECT_URL']);
-            die();
-        }
+        // if (!$this->isGranted('Editor')) {
+        //     header('Http/1.1 302');
+        //     header('Location: /login?required_page=' . $_SERVER['REDIRECT_URL']);
+        //     die();
+        // }
     }
 
-    function index($request)
+    /**
+     * GET:  fetch the markdown for a file
+     * POST: save edited file
+     */
+    function edit($request)
     {
-        return $this->render('admin/admin.twig');
+        $strPage = '/' . $_REQUEST['page'];
+        $page = $this->nacho->getPage($strPage);
+
+        if (strtolower($request->requestMethod) === 'post') {
+            if (!$page || !is_file($page['file'])) {
+                return $this->json(['message' => 'Unable to find this file']);
+            }
+
+            file_put_contents($page['file'], $_REQUEST['content']);
+            return $this->json(['message' => 'successfully saved content']);
+        }
+
+        return $this->json($page);
     }
 
     public function delete($request)
@@ -71,56 +87,6 @@ class AdminController extends AbstractController
         returnHome();
     }
 
-    public function editHome($request)
-    {
-        $imagesDir = $request->documentRoot . '/images/home';
-        $fileNames = ['original', 'cover', 'banner'];
-        $images = [];
-        $cropData = [];
-        foreach (MONTHS as $key => $month) {
-            $tmpImages = [];
-            if (!is_dir("${imagesDir}/${key}_${month}")) {
-                mkdir("${imagesDir}/${key}_${month}", 0777, true);
-                file_put_contents("${imagesDir}/${key}_${month}/.gitignore", "*.jpg\n*.json");
-                file_put_contents("${imagesDir}/${key}_${month}/crop-data.json", "{}");
-            }
-            foreach ($fileNames as $name) {
-                if (is_file("${imagesDir}/${key}_${month}/${name}.jpg")) {
-                    $tmpImages[$name] = "/images/home/${key}_${month}/${name}.jpg";
-                }
-            }
-            $images[$month] = $tmpImages;
-            $cropData[$month] = file_get_contents("${imagesDir}/${key}_${month}/crop-data.json");
-        }
-
-        // if this is a post request, store new files
-        if (strtoupper($request->requestMethod) === 'POST') {
-            $imageHelper = new ImageHelper();
-            $month = $_REQUEST['month'];
-            $key = array_search($month, MONTHS);
-            $cover = $_REQUEST['cover'];
-            $banner = $_REQUEST['banner'];
-            $cropCover = json_decode($_REQUEST['cropCover'], true);
-            $cropBanner = json_decode($_REQUEST['cropBanner'], true);
-            $cropData = ['cover' => $cropCover, 'banner' => $cropBanner];
-            file_put_contents("${imagesDir}/${key}_${month}/original.jpg", file_get_contents("${imagesDir}/${key}_${month}/tmp.original.jpg"));
-            file_put_contents("${imagesDir}/${key}_${month}/crop-data.json", json_encode($cropData));
-            $coverName = md5(random_bytes(10));
-            $bannerName = md5(random_bytes(10));
-            file_put_contents("/tmp/${coverName}.jpeg", ImageHelper::base64_to_jpeg($cover));
-            file_put_contents("/tmp/${bannerName}.jpeg", ImageHelper::base64_to_jpeg($banner));
-            $imageHelper->compressImage("/tmp/${coverName}.jpeg", 200, "${imagesDir}/${key}_${month}", 'cover.jpg');
-            $imageHelper->compressImage("/tmp/${bannerName}.jpeg", 1080, "${imagesDir}/${key}_${month}", 'banner.jpg');
-        }
-
-
-        return $this->render('admin/edit-home.twig', [
-            'months' => MONTHS,
-            'files' => $images,
-            'cropData' => $cropData,
-        ]);
-    }
-
     public function uploadOriginal($request)
     {
         if (strtoupper($request->requestMethod) !== 'POST') {
@@ -135,33 +101,6 @@ class AdminController extends AbstractController
         $imageHelper->compressImage($original['tmp_name'], 1000, "${imagesDir}/${key}_${month}", 'tmp.original.jpg');
 
         return $this->json(['file' => "/images/home/${key}_${month}/tmp.original.jpg"]);
-    }
-
-    public function add($request)
-    {
-        if (strtolower($request->requestMethod) === 'post') {
-            $timeFileName = new DateTime($_REQUEST['journalEntry']);
-            $fileName = $timeFileName->format('Y-m-d');
-            $month = $timeFileName->format('F');
-            $date = new DateTime();
-            $content =
-                "---\ntitle: " .
-                $fileName .
-                "\ndate: " .
-                $date->format('Y-m-d H:i') .
-                "\n---";
-            file_put_contents($_SERVER['DOCUMENT_ROOT'] . "/content/" . $month . '/' . $timeFileName->format('d.m.Y') . '.md', $content);
-            header('Location: /admin/edit?file=/' . $month . '/' . $timeFileName->format('d.m.Y'));
-            header('HTTP/1.1 302');
-        }
-
-        $now = new \DateTime();
-
-        return $this->render('admin/add.twig', [
-            'parent' => $_REQUEST['parent'],
-            'pages' => MONTHS,
-            'currentMonth' => intval($now->format('n')) - 1,
-        ]);
     }
 
     public function editCurrent($request)
@@ -185,17 +124,6 @@ class AdminController extends AbstractController
         file_put_contents("${contentDir}/${fileName}", $content);
 
         return $this->json(['message' => 'Successfully added status']);
-    }
-
-    public function testExif($request)
-    {
-        if (strtoupper($request->requestMethod) === 'POST') {
-            print_r($_FILES);
-            $file = $_FILES['file'];
-            print_r(exif_read_data($file['tmp_name']));
-        }
-
-        return $this->render('admin/test-exif.twig');
     }
 
     public function appendImage($request)
@@ -236,29 +164,6 @@ class AdminController extends AbstractController
         }
 
         return "/${month}/${title}";
-    }
-
-    function edit($request)
-    {
-        $page = $this->nacho->getPage($_REQUEST['file']);
-
-        if ($request->requestMethod === 'POST') {
-            header('content-type: application/json');
-            if (!$page || !is_file($page['file'])) {
-                return $this->json(['message' => 'Unable to find this file']);
-            }
-
-            file_put_contents($page['file'], $_REQUEST['content']);
-            return json_encode(['message' => 'successfully saved content']);
-        }
-
-        $month = explode('/', $page['id'])[1];
-
-        return $this->render('admin/edit.twig', [
-            'page' => $page,
-            'referer' => $_SERVER['HTTP_REFERER'],
-            'month' => $month,
-        ]);
     }
 
     public function showInfo($request)
