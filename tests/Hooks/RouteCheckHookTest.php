c1e43565361905dddd73b44dd82e00e97f1fa602
ChristianGroeber <swiss8oy.chg@gmail.com>
2023-03-27 16:39:04 +0200

    first test cases

---
+++ tests/Hooks/RouteCheckHookTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Hooks;
+
+use App\Controllers\FrontendController;
+use App\Hooks\RouteCheckHook;
+use Nacho\Models\Route;
+use PHPUnit\Framework\TestCase;
+
+class RouteCheckHookTest extends TestCase
+{
+    public function testCallChangesControllerForNonApiRoutes()
+    {
+        $hook = new RouteCheckHook();
+        $route = new Route(['route' => 'home', 'controller' => 'HomeController', 'function' => 'index']);
+
+        $newRoute = $hook->call($route);
+
+        $this->assertEquals(FrontendController::class, $newRoute->getController());
+    }
+
+    public function testCallDoesNotChangeControllerForApiRoutes()
+    {
+        $hook = new RouteCheckHook();
+        $route = new Route(['route' => '/api/users', 'controller' => 'UserController', 'function' => 'index']);
+
+        $newRoute = $hook->call($route);
+
+        $this->assertEquals('UserController', $newRoute->getController());
+        $this->assertEquals('index', $newRoute->getFunction());
+        $this->assertEquals('UserController', $newRoute->getController());
+    }
+}
+
