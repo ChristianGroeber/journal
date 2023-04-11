88d42c693dc0bea94ab0c5042aee72676697065e
ChristianGroeber <swiss8oy.chg@gmail.com>
2023-04-11 12:28:14 +0200

    uncomment

--- app/App.vue
+++ app/App.vue
@@ -37,7 +37,7 @@ export default {
   created() {
     this.$store.dispatch("getToken");
     this.$store.dispatch("getEntries").then(() =>  {
-      // resizeVideos();
+      resizeVideos();
     });
     this.axios.interceptors.request.use(
       (config) => {
