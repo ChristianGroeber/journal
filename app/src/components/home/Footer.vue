118a516fe11ac24201d3ed02cd224908acf7fb2e
ChristianGroeber <christian.groeber@bluewin.ch>
2023-04-13 19:01:35 +0200

    footer update, fixes #22

--- app/src/components/home/Footer.vue
+++ app/src/components/home/Footer.vue
@@ -1,6 +1,16 @@
 <template>
   <footer>
-    PixlJournal Version {{ version }}
+    <span>
+      PixlJournal Version {{ version }}
+    </span>
+    |
+    <span>
+      <a href="/auth/login">Login</a>
+    </span>
+    |
+    <span>
+      <a href="https://github.com/ChristianGroeber/journal">GitHub</a>
+    </span>
   </footer>
 </template>
 
@@ -20,5 +30,6 @@ export default {
 footer {
   text-align: center;
   font-size: 0.8rem;
+  margin: 1rem auto;
 }
 </style>
