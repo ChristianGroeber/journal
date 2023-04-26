93131bc467eae06e6930fb34ac85e400c9dc38fc
ChristianGroeber <swiss8oy.chg@gmail.com>
2023-04-26 15:59:43 +0200

    extracted loader css

--- app/src/components/Loading.vue
+++ app/src/components/Loading.vue
@@ -12,17 +12,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-.loader {
-  display: block;
-  position: fixed;
-  height: 3px;
-  width: 0;
-  top: 0;
-  background-color: red;
-  z-index: 100;
-  border-top-right-radius: 1px;
-  border-top-left-radius: 1px;
-}
-</style>
