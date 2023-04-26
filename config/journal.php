b03868a287e8ff4ff02617b00d96edae76b18d87
ChristianGroeber <swiss8oy.chg@gmail.com>
2023-04-26 20:33:50 +0200

    v2023.5

--- config/journal.php
+++ config/journal.php
@@ -4,5 +4,5 @@ return [
     'year' => 2023,
     'mediaDir' => $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'media',
     'mediaBaseUrl' => DIRECTORY_SEPARATOR . 'media',
-    'version' => '2023.4',
+    'version' => '2023.5',
 ];
