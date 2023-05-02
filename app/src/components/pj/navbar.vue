cd1085ba6c69746f936a9b1d9f05178d029114ca
ChristianGroeber <christian.groeber@bluewin.ch>
2023-05-02 22:25:45 +0200

    minor change

--- app/src/components/pj/navbar.vue
+++ app/src/components/pj/navbar.vue
@@ -1,9 +1,10 @@
 <template>
-  <el-menu mode="horizontal">
-    <el-menu-item v-for="(item, index) in nav" :index="(index + 1).toString()" :key="index" @click="handleClick(index)">
-      {{ item.label }}
-    </el-menu-item>
-  </el-menu>
+    <el-menu mode="horizontal">
+        <el-menu-item v-for="(item, index) in nav" :index="(index + 1).toString()" :key="index"
+                      @click="handleClick(index)">
+            {{ item.label }}
+        </el-menu-item>
+    </el-menu>
 </template>
 
 <script lang="ts">
@@ -11,24 +12,30 @@ import {defineComponent} from "vue";
 import {useRouter} from "vue-router";
 
 export default defineComponent({
-  name: "Navbar",
-  props: ['nav'],
-  data: function () {
-    return {
-      router: useRouter(),
-    };
-  },
-  methods: {
-    handleClick(itemId: number) {
-      const item = this.nav[itemId];
-      if (item.func !== undefined) {
-        item.func();
-      } else if ("page" in item) {
-        this.router.push(item.page);
-      } else {
-        console.error('I don\'t know what to do with item #' + itemId);
-      }
+    name: "Navbar",
+    props: ['nav'],
+    data: function () {
+        return {
+            router: useRouter(),
+        };
     },
-  },
+    methods: {
+        handleClick(itemId: number) {
+            const item = this.nav[itemId];
+            if (item.func !== undefined) {
+                item.func();
+            } else if ("page" in item) {
+                this.router.push(item.page);
+            } else {
+                console.error('I don\'t know what to do with item #' + itemId);
+            }
+        },
+    },
 })
 </script>
+
+<style>
+.el-menu--horizontal {
+    border-bottom: none !important;
+}
+</style>
