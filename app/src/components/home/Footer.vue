cfb4509ab4fbb71c59a9ec49c28f05b6297a89a1
Christian Gröber <christian.groeber@bluewin.ch>
2023-12-27 17:52:47 +0100

    logging out

--- app/src/components/home/Footer.vue
+++ app/src/components/home/Footer.vue
@@ -1,7 +1,10 @@
 <template>
     <footer>
         <span>PixlJournal Version {{ version }}</span>|
-        <span><el-link @click="showLoginForm">Login</el-link></span>|
+        <span>
+            <el-link v-if="!isLoggedIn" @click="showLoginForm">Login</el-link>
+            <el-link v-else @click="logout">Logout</el-link>
+        </span>|
         <span><a href="https://github.com/ChristianGroeber/journal">GitHub</a></span>|
         <span><el-link @click="switchTheme" :icon="themeIcon"></el-link></span>
     </footer>
@@ -11,12 +14,14 @@
 import {defineComponent} from "vue";
 import {useMainStore} from "@/src/store/main";
 import {Moon, Sunny} from "@element-plus/icons-vue";
+import {useAuthStore} from "@/src/store/auth";
 
 export default defineComponent({
     name: "Footer",
     data() {
         return {
             mainStore: useMainStore(),
+            authStore: useAuthStore(),
         }
     },
     methods: {
@@ -28,11 +33,17 @@ export default defineComponent({
             const newTheme = selectedTheme === 'dark' ? 'light' : 'dark';
 
             this.mainStore.setTheme(newTheme);
-        }
+        },
+        logout() {
+            this.authStore.logout();
+        },
     },
     computed: {
         version() {
             return this.mainStore.getMeta.journalVersion;
+        },
+        isLoggedIn() {
+            return this.authStore.getToken !== null;
         },
         themeIcon() {
             const selectedTheme = this.mainStore.getTheme;
