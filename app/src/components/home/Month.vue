a9a5fea360c9cd6b209896727ee14354ebd5ece9
ChristianGroeber <swiss8oy.chg@gmail.com>
2024-07-22 20:08:33 +0200

    better styling

--- app/src/components/home/Month.vue
+++ app/src/components/home/Month.vue
@@ -2,9 +2,9 @@
     <div>
         <h2>{{ month.name }}</h2>
         <div class="article-list container">
-            <div v-for="(day, index) in month.days" :key="index">
+            <template v-for="(day, index) in month.days" :key="index">
                 <Day :day="day"/>
-            </div>
+            </template>
         </div>
     </div>
 </template>
@@ -21,17 +21,3 @@ export default defineComponent({
     },
 })
 </script>
-
-<style lang="scss">
-@import '/app/style/variables.scss';
-
-h2 {
-    text-align: center;
-    padding: 5px 0;
-    margin: 5px 0 20px 0 !important;
-    background-color: rgba(107, 81, 81, 0.33);
-    color: white;
-    border: $black-border;
-    text-shadow: 0 0 2px black;
-}
-</style>
--- app/src/components/home/RaceReport.vue
+++ app/src/components/home/RaceReport.vue
@@ -76,8 +76,13 @@ export default defineComponent({
 .rr_card {
     background-repeat: no-repeat;
     background-size: cover;
-    padding: 3rem;
+    padding: 3rem 3em 1.5em 3em;
     box-sizing: border-box;
+    background-color: #ffffff;
+    border: 4px solid #000000;
+    padding: 20px;
+    margin: 20px;
+    box-shadow: 10px 10px 0 #333333;
 }
 .rr_data_table, .rr_distance {
     text-align: center;
@@ -91,14 +96,54 @@ export default defineComponent({
 .rr_distance {
     font-size: 3rem;
     margin: 0;
-}
+    // color: #d90429;
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+    gap: 10px;
 
-.rr_distance_subtitle {
-    margin: 0;
+    svg {
+        fill: #2b2d42;
+        width: 50px;
+        height: 50px;
+    }
 }
 
 .rr_data_table {
     display: flex;
-    gap: 1rem;
+    flex-wrap: wrap;
+    gap: 20px;
+}
+
+/* Data value */
+.rr_data_value {
+    background-color: #edf2f4;
+    border: 3px solid #000000;
+    padding: 10px;
+    box-shadow: 5px 5px 0 #333333;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 1.2rem;
+    color: #000000;
+
+    svg {
+        fill: #d90429;
+        // color: #d90429;
+        width: 25px;
+        height: 25px;
+    }
+
+    &:nth-child(even) {
+        background-color: #bde0fe;
+        border-color: #669bbc;
+        box-shadow: 5px 5px 0 #3b6275;
+    }
+
+    &:nth-child(odd) {
+        background-color: #ffafcc;
+        border-color: #ff7096;
+        box-shadow: 5px 5px 0 #cc4f73;
+    }
 }
 </style>
